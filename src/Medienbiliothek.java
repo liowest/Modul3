133ab590c12a579c22749780553c98c0ee32722f
lionel.westreicher <lionel.westreicher@gmail.com>
2024-10-03 15:05:46 +0200

    in Medienbiliothek eine arraylist ertsellt unf mit der emthode durchlaufen alle medien durchgegangen und angezeigt

--- src/Medienbiliothek.java
+++ src/Medienbiliothek.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Medienbiliothek {
+    private ArrayList<Medium> medien;
+
+    public void durchlaufen(){
+       for(Medium medium : medien){
+           medium.anzeigen();
+       }
+    }
+
+    public void medienhinzufügen(Medium medium) {
+        medien.add(medium);
+
+    }
 }
